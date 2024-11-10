92edd45b4cd415b83bca0ccea9ab84f19228c42a
Allan Antunes <allan.antunes11@outlook.com>
2024-11-10 05:52:40 -0300

    :smile: Conversations improvements

--- agent.py
+++ agent.py
@@ -4,23 +4,24 @@ import json
 import os
 
 class agent:
-    def __init__(self, space_id):
-        self.__MODEL = "llama-3.2-90b-vision-preview"
+    def __init__(self,space_id):
+        self.__MODEL="llama-3.2-90b-vision-preview"
         self.__space_id = space_id
-        self.__space_item_id: str
+        self.__space_item_id : str
 
-    def __pegar_info_expo(self):
+    def __pegar_info_expo(self,a):
         headers = {'Authorization': request.headers.get('Authorization')}
         response = requests.get(f"https://api.acessibilidade.tec.br/space/{self.__space_id}", headers=headers)
         return response.text
 
-    def __pegar_info_item(self):
+    def __pegar_info_item(self,a):
         headers = {'Authorization': request.headers.get('Authorization')}
         response = requests.get(f"https://api.acessibilidade.tec.br/space/{self.__space_id}/item/", headers=headers)
         return response.text
-
+    
     def run_conversation(self, user_prompt, groq_client):
-        messages = [
+        
+        messages=[
             {
                 "role": "system",
                 "content": """Você é o Curador.ia, uma IA que fornece informações sobre exposições de arte e responde as perguntas que venham no texto.
@@ -42,20 +43,30 @@ class agent:
                     "description": "Retorna a descrição das exposição",
                     "parameters": {
                         "type": "object",
-                        "properties": {},
-                        "required": [],
+                        "properties": {
+                            "a": {
+                                "type": "string",
+                                "description": "Literalmente nada.",
+                            }
+                        },
+                        "required": ["a"],
                     },
                 },
             },
-            {
+                        {
                 "type": "function",
                 "function": {
                     "name": "__pegar_info_item",
                     "description": "Retorna a descrição dos itens da exposição",
                     "parameters": {
                         "type": "object",
-                        "properties": {},
-                        "required": [],
+                        "properties": {
+                            "a": {
+                                "type": "string",
+                                "description": "Literalmente nada",
+                            }
+                        },
+                        "required": ["a"],
                     },
                 },
             }
@@ -71,6 +82,7 @@ class agent:
         )
 
         response_message = response.choices[0].message
+
         tool_calls = response_message.tool_calls
 
         if tool_calls:
@@ -84,7 +96,8 @@ class agent:
             for tool_call in tool_calls:
                 function_name = tool_call.function.name
                 function_to_call = available_functions[function_name]
-                function_response = function_to_call()
+                function_args = json.loads(tool_call.function.arguments)
+                function_response = function_to_call(**function_args)
                 messages.append(
                     {
                         "tool_call_id": tool_call.id,
