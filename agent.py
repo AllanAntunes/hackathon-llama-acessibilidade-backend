4529c57f24b284568b9618714158ea831543d34c
Allan Antunes <allan.antunes11@outlook.com>
2024-11-10 05:48:02 -0300

    :smile: Conversations improvements

--- agent.py
+++ agent.py
@@ -4,10 +4,10 @@ import json
 import os
 
 class agent:
-    def __init__(self,space_id):
-        self.__MODEL="llama-3.2-90b-vision-preview"
+    def __init__(self, space_id):
+        self.__MODEL = "llama-3.2-90b-vision-preview"
         self.__space_id = space_id
-        self.__space_item_id : str
+        self.__space_item_id: str
 
     def __pegar_info_expo(self):
         headers = {'Authorization': request.headers.get('Authorization')}
@@ -18,17 +18,14 @@ class agent:
         headers = {'Authorization': request.headers.get('Authorization')}
         response = requests.get(f"https://api.acessibilidade.tec.br/space/{self.__space_id}/item/", headers=headers)
         return response.text
-    
+
     def run_conversation(self, user_prompt, groq_client):
-        
-        messages=[
+        messages = [
             {
                 "role": "system",
                 "content": """Você é o Curador.ia, uma IA que fornece informações sobre exposições de arte e responde as perguntas que venham no texto.
                 O texto será extraído de um áudio, portanto pode haver erros, mesmo assim tente ser informativo enquanto formula a resposta.
-                Use a função __pegar_info_expo para receber informações relevantes sobre as exposições.
-                Use a função __pegar_info_item para receber as informações sobre os itens da exposição.
-                Sua resposta será transformada em áudio, então faça parecer como uma conversa.
+                Use a função __pegar_info_expo para receber informações relevantes sobre as exposições. Sua resposta será transformada em áudio, então faça parecer como uma conversa.
                 É importante ter como resposta somente o que vai pro áudio, não tenha introduções, a não ser que o usuário diga.
                             """
             },
@@ -43,13 +40,23 @@ class agent:
                 "function": {
                     "name": "__pegar_info_expo",
                     "description": "Retorna a descrição das exposição",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {},
+                        "required": [],
+                    },
                 },
             },
-                        {
+            {
                 "type": "function",
                 "function": {
                     "name": "__pegar_info_item",
                     "description": "Retorna a descrição dos itens da exposição",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {},
+                        "required": [],
+                    },
                 },
             }
         ]
@@ -64,7 +71,6 @@ class agent:
         )
 
         response_message = response.choices[0].message
-
         tool_calls = response_message.tool_calls
 
         if tool_calls:
@@ -78,11 +84,13 @@ class agent:
             for tool_call in tool_calls:
                 function_name = tool_call.function.name
                 function_to_call = available_functions[function_name]
+                function_response = function_to_call()
                 messages.append(
                     {
                         "tool_call_id": tool_call.id,
                         "role": "tool",
                         "name": function_name,
+                        "content": function_response,
                     }
                 )
 
